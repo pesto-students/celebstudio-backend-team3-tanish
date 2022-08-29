a9b059c271dda4733b87b98a72f058dd3fd9b767
Jitender Prasad <teejzit@gmail.com>
2022-08-29 12:27:51 +0530

    removed file

--- router/loginRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const handleLogin = require('../handler/loginHandler')
-
-
-router.post('/influencer', handleLogin.handleInfluencerLogin )
-router.post('/business', handleLogin.handleBusinessLogin )
-
-module.exports = router;
--- router/signupRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const handleLogin = require('../handler/signupHandler')
-
-
-router.post('/influencer', handleLogin.handleInfluencerSignup )
-router.post('/business', handleLogin.handleBusinessSignup )
-
-module.exports = router;
